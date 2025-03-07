0b7efcdf150a1f2b224e76ff3b22fb38abd6c430
azrulazib79 <azrulazib79@gmail.com>
2025-03-07 16:24:45 +0000

    signup

--- signup.php
+++ signup.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             height: 100vh;
         }
         .navbar {
-            width: 95%;
+            width: 97%;
             padding: 10px 30px;
             display: flex;
             justify-content: space-between;
