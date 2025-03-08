04bd9e3f26e4b562d04894c6fb7f13980fca6f0c
azrulazib79 <azrulazib79@gmail.com>
2025-03-08 08:04:30 +0000

    signup

--- signup.php
+++ signup.php
@@ -110,7 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             margin-bottom: 20px;
         }
         input {
-            width: calc(90%);
+            width: calc(95%);
             padding: 10px;
             margin: 10px 0;
             border: none;
